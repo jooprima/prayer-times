95373e17d6f092f2ffb781939015fb2e271fead4
jooprima <jooprima@gmail.com>
2020-10-28 23:54:03 +0700

    fetch API Aladhan

--- app.js
+++ app.js
@@ -1,27 +1,37 @@
-function success(position){
-    console.log(position);
+function prayerTimes(latitude, longitude) {
+  fetch(
+    'http://api.aladhan.com/v1/calendar?latitude='+latitude+'&longitude='+longitude+'&method=2'
+  )
+    .then((response) => response.json())
+    .then(function (response) {
+      console.log(response.data[0].timings);
+    });
 }
 
-function error(){
-    alert('Posisi tidak dapat diakses');
-};
-
-function userLocation(){
-    if(!navigator.geolocation){
-        alert('Geolocation tidak didukung di browser anda,gunakan browser lain')
-    }else{
-        navigator.geolocation.getCurrentPosition(success,error);
-    }
+function success(position) {
+  prayerTimes(position.coords.latitude, position.coords.longitude);
 }
 
-function index(){
-    let app = document.getElementById('app');
-    let h3 = document.createElement('h3');
-    h3.innerHTML = 'Prayer Times';
+function error() {
+  alert("Posisi tidak dapat diakses");
+}
 
-    app.appendChild(h3);
+function userLocation() {
+  if (!navigator.geolocation) {
+    alert("Geolocation tidak didukung di browser anda,gunakan browser lain");
+  } else {
+    navigator.geolocation.getCurrentPosition(success, error);
+  }
+}
 
-    userLocation();
+function index() {
+  let app = document.getElementById("app");
+  let h3 = document.createElement("h3");
+  h3.innerHTML = "Prayer Times";
+
+  app.appendChild(h3);
+
+  userLocation();
 }
 
 index();
